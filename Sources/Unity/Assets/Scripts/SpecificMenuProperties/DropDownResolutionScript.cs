67f8b0d317cd3fd1c9b9176bd893edd9234fcd98
Lefevre-Florian <72788913+LefevreFlorian@users.noreply.github.com>
2022-01-27 16:44:21 +0100

    Functional screen resolution system

--- Sources/Unity/Assets/Scripts/SpecificMenuProperties/DropDownResolutionScript.cs
+++ Sources/Unity/Assets/Scripts/SpecificMenuProperties/DropDownResolutionScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,6 +6,23 @@ using UnityEngine.UI;
 public class DropDownResolutionScript : MonoBehaviour
 {
     public Dropdown dropdown;
+
+    private void Start()
+    {
+        initResolution();
+    }
+
+    private void initResolution()
+    {
+        if (PlayerPrefs.HasKey("ScreenWidth") && PlayerPrefs.HasKey("ScreenHeight"))
+        {
+            Screen.SetResolution(PlayerPrefs.GetInt("ScreenWidth"),PlayerPrefs.GetInt("ScreenHeight"),true);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.currentResolution.width,Screen.currentResolution.height,true);
+        }
+    }
     
     private void onResolutionScreen()
     {
@@ -20,7 +38,19 @@ public class DropDownResolutionScript : MonoBehaviour
         dropdown.AddOptions(tmpL);
     }
 
-    public void saveScreenSetting()
+    public void saveScreenSettings()
+    {
+        string res = dropdown.options[dropdown.value].text;
+        string[] tmp = res.Split(char.Parse("x"));
+
+        int width = int.Parse(tmp[0]);
+        int height = int.Parse(tmp[1]);
+        PlayerPrefs.SetInt("ScreenWidth", width);
+        PlayerPrefs.SetInt("ScreenHeight", height);
+        PlayerPrefs.Save();
+    }
+
+    private void updateScreenSetting()
     {
         string res = dropdown.options[dropdown.value].text;
         string[] tmp = res.Split(char.Parse("x"));
@@ -29,11 +59,23 @@ public class DropDownResolutionScript : MonoBehaviour
         int height = int.Parse(tmp[1]);
         Screen.SetResolution(width,height,true);
     }
-
-    void Start()
+    
+    private void OnEnable()
     {
-        Screen.fullScreen = !Screen.fullScreen;
         onResolutionScreen();
+        dropdown.onValueChanged.AddListener(delegate
+        {
+            updateScreenSetting();
+        });
+    }
+    
+    // -------- Test ------------ //
+
+    // For testing purpose only
+
+    private void currentResolution()
+    {
+        Debug.Log(Screen.currentResolution);
     }
     
 }
