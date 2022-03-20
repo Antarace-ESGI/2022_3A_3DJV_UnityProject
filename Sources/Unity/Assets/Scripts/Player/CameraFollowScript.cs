46b544bff48d8c37e28f718a2b6dee1a3f4a3c77
Erwan <erwan.legloannec@outlook.com>
2022-03-20 11:34:09 +0100

    fix: camera position

--- Sources/Unity/Assets/Scripts/Player/CameraFollowScript.cs
+++ Sources/Unity/Assets/Scripts/Player/CameraFollowScript.cs
@@ -3,37 +3,17 @@ using UnityEngine;
 public class CameraFollowScript : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset;
     public float timeOffset = 0.3f;
-    public Rigidbody body;
     public float cameraDistance = 5f;
     public float floatDistance = 2f;
 
     private Vector3 velocity;
     private float fovVelocity;
 
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
-
     void LateUpdate()
     {
         Vector3 position = player.transform.position - player.transform.forward * cameraDistance;
         position.y += floatDistance;
-
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, floatDistance, layerMask))
-        {
-            position.y = hit.transform.position.y + floatDistance;
-        }
 
         transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, timeOffset);
         transform.LookAt(player.transform);
