917ea470c740a00b3afcdd8843ec908f20dcfeeb
Erwan <erwan.legloannec@outlook.com>
2022-03-27 10:52:28 +0200

    add: vehicle types

---
+++ Sources/Unity/Assets/Scripts/Player/VehicleLoader.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class VehicleLoader : MonoBehaviour
+    {
+        private static bool _initialized;
+        
+        public ShipController shipController;
+        public Material bodyMaterial;
+        [Range(0, 3)] public int vehicleIndex;
+
+        public VehicleLoader()
+        {
+            if (!_initialized)
+            {
+                Vehicle.CreateVehicle("RedPlayer", Color.blue, 10, 2, 2, 5);
+                Vehicle.CreateVehicle("Quic", Color.red, 15, 1.5f, 2, 5);
+                Vehicle.CreateVehicle("Boosty", Color.yellow, 5, 5, 2, 5);
+                Vehicle.CreateVehicle("Agil", Color.green, 10, 10, 1, 10);
+            }
+
+            _initialized = true;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Vehicle vehicle = Vehicle.Vehicles[vehicleIndex];
+
+            shipController.baseThrottle = vehicle.baseThrottle;
+            shipController.boostMultiplier = vehicle.boostMultiplier;
+            shipController.boostDuration = vehicle.boostDuration;
+            shipController.yawStrength = vehicle.yawStrength;
+
+            bodyMaterial.color = vehicle.color;
+        }
+    }
+}
