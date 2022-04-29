84554ab429de15c6f003778641d8c7fa738fbb7c
Erwan <erwan.legloannec@outlook.com>
2022-04-29 19:53:10 +0200

    j'ai tout cassé

---
+++ Sources/Unity/Assets/Scripts/Checkpoints/CheckpointController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace Checkpoints
+{
+    /// <summary>
+    /// This class handles checkpoint increment by passing inside a checkpoint
+    /// as well as the player entities' (Players and AIs) respawns.
+    /// It also provides an interface for getting and setting checkpoints' related values.
+    /// </summary>
+    public class CheckpointController : MonoBehaviour
+    {
+        public GameObject[] checkpoints;
+
+        public int _checkpointIndex = 0;
+        private GameObject _nextCheckpoint, _currentCheckpoint;
+
+        private void Start()
+        {
+            _currentCheckpoint = _nextCheckpoint = checkpoints[_checkpointIndex];
+        }
+
+        private void IncrementCheckpoint(GameObject currentCheckpoint)
+        {
+            if (currentCheckpoint.Equals(_nextCheckpoint))
+            {
+                _currentCheckpoint = checkpoints[_checkpointIndex];
+                _checkpointIndex = Mathf.Min(_checkpointIndex + 1, checkpoints.Length - 1);
+                _nextCheckpoint = checkpoints[_checkpointIndex];
+            }
+        }
+
+        public void OnTriggerEnter(Collider collision)
+        {
+            IncrementCheckpoint(collision.gameObject);
+        }
+        
+        public void DecrementCheckpoint()
+        {
+            _currentCheckpoint = checkpoints[_checkpointIndex];
+            _checkpointIndex = Mathf.Max(_checkpointIndex - 1, 0);
+            _nextCheckpoint = checkpoints[_checkpointIndex];
+        }
+
+        public GameObject GetCurrentCheckpoint()
+        {
+            Debug.Log(String.Format("{0}, {1}", _currentCheckpoint.name, _nextCheckpoint.name));
+            return _currentCheckpoint;
+        }
+
+        public GameObject GetNextCheckpoint()
+        {
+            Debug.Log(String.Format("{0}, {1}", _currentCheckpoint.name, _nextCheckpoint.name));
+            return _nextCheckpoint;
+        }
+
+        public void RespawnEntity()
+        {
+            transform.position = GetCurrentCheckpoint().transform.position;
+        }
+    }
+}
