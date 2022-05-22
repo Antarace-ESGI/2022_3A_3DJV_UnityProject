5abf966f2dad0a0cd29a2388460a13ad9cf88d38
Lefevre-Florian <72788913+LefevreFlorian@users.noreply.github.com>
2022-05-22 14:59:25 +0200

    Add prefab

--- Sources/Unity/Assets/Scripts/Ai/AINode.cs
+++ Sources/Unity/Assets/Scripts/Ai/AINode.cs
@@ -1,23 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class AINode : MonoBehaviour
 {
 
     [Header("Node")] 
-    [SerializeField] private GameObject rChild;
-    [SerializeField] private GameObject lChild;
+    [SerializeField] [CanBeNull] private GameObject rChild;
+    [SerializeField] [CanBeNull] private GameObject lChild;
     
-    public GameObject? GetrChild()
+    public GameObject GetrChild()
     {
         if(rChild)
             return rChild;
         return null;
     }
-
-    public GameObject? GetlChild()
+    
+    public GameObject GetlChild()
     {
         if(lChild)
             return lChild;
