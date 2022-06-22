d748afcaba013a150c60042d3a35bebd9f531125
Lefevre-Florian <72788913+LefevreFlorian@users.noreply.github.com>
2022-06-22 16:27:33 +0200

    Add Gamepad and KeyboardMouse navigation

---
+++ Sources/Unity/Assets/Scripts/Menu/ControlProperties/UISelectorScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISelectorScript : MonoBehaviour
+{
+    // Just a script to set select button for the Gamepad option
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(this.gameObject);
+    }
+}
