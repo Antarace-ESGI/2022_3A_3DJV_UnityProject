f699eaca5b56dc646e39ad6b4d3fec046c7500d0
Erwan <erwan.legloannec@outlook.com>
2022-07-08 18:28:53 +0200

    fix: ships being one in another

--- Sources/Unity/Assets/Scripts/Menu/VehiculeSelectionProperties/PlayerSelectionScript.cs
+++ Sources/Unity/Assets/Scripts/Menu/VehiculeSelectionProperties/PlayerSelectionScript.cs
@@ -30,7 +30,11 @@ public class PlayerSelectionScript : MonoBehaviour
 
     private void PlayerInputManagerOnPlayerJoined(PlayerInput input)
     {
-        var player = input.gameObject.GetComponent<WaitForAll>();
+        // Move player a bit from the other
+        var inputGameObject = input.gameObject;
+        inputGameObject.transform.position = new Vector3(input.playerIndex * 50, 0, 0);
+
+        var player = inputGameObject.GetComponent<WaitForAll>();
         player.playerSelection = this;
         player.input = input;
         SelectedVehiclesScript.SetDeviceForPlayer(input.playerIndex, input.devices[0]); // Unsafe?
