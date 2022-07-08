03505ef7c36d05d4f8cd00e3b2fe2e7d8b88f1d1
Erwan <erwan.legloannec@outlook.com>
2022-07-08 17:32:12 +0200

    add: place players and ais on start line

---
+++ Sources/Unity/Assets/Scripts/Checkpoints/SpawnScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Checkpoints
+{
+    [RequireComponent(typeof(CheckpointController))]
+    public class SpawnScript : MonoBehaviour
+    {
+        private static int _spawnedPlayerCount;
+
+        private void Start()
+        {
+            SpawnPlayer();
+        }
+
+        /// <summary>
+        /// Teleports the player to spawn on first apparition
+        /// </summary>
+        private void SpawnPlayer()
+        {
+            var spawnPoints = CheckpointController.GetSpawnCheckpoint().GetComponentsInChildren<Transform>();
+            var startTransform = spawnPoints[_spawnedPlayerCount++]; // Unsafe ?
+            transform.position = startTransform.position;
+            transform.rotation = startTransform.rotation;
+        }
+    }
+}
