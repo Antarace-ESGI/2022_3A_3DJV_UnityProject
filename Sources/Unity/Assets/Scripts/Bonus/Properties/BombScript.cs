3c29ed5a80048dbeefa2ad7fe35feb450b076c67
Matthieu39 <matthieulinel99@gmail.com>
2022-07-16 20:17:16 +0200

    Bomb Sound

--- Sources/Unity/Assets/Scripts/Bonus/Properties/BombScript.cs
+++ Sources/Unity/Assets/Scripts/Bonus/Properties/BombScript.cs
@@ -9,17 +9,20 @@ public class BombScript : MonoBehaviour
     [SerializeField] private int damage = 50;
     [SerializeField] private GameObject explosionArea;
 
+    public AudioClip ExplosionSound;
+    public AudioSource audioSource;
+
     private GameObject _user;
 
     public void SetUser(GameObject user)
     {
         _user = user;
     }
-    
+
     void Start()
     {
         float time = timer;
-        
+
         gameObject.transform.position = _user.transform.position;
 
         StartCoroutine(PrintArea(time - 2.0f));
@@ -29,26 +32,28 @@ public class BombScript : MonoBehaviour
     IEnumerator PrintArea(float time)
     {
         yield return new WaitForSeconds(time);
-        
+
         explosionArea.transform.localScale = new Vector3(radius, radius, radius);
         explosionArea.SetActive(true);
-        
+
     }
-    
+
     IEnumerator Explode(float time)
     {
         yield return new WaitForSeconds(time);
-        
+
         var center = _user.transform.position;
 
         Collider[] colliders = Physics.OverlapSphere(center, radius);
+        audioSource.clip = ExplosionSound;
+        audioSource.Play();
         foreach (Collider collider in colliders)
         {
             if(collider.CompareTag("Player") || collider.CompareTag("AI"))
                 collider.gameObject.GetComponent<PlayerStatsScript>().healthPoint -= damage;
         }
-        
+        yield return new WaitForSeconds(3f);
         Destroy(gameObject);
-        
+
     }
 }
