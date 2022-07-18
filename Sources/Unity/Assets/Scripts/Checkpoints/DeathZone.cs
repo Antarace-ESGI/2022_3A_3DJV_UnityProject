3d36e77833a4f9631d117199435bbfad4c69eb89
Matthieu39 <matthieulinel99@gmail.com>
2022-07-19 00:28:46 +0200

    Respawn Sound

--- Sources/Unity/Assets/Scripts/Checkpoints/DeathZone.cs
+++ Sources/Unity/Assets/Scripts/Checkpoints/DeathZone.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
+
+  public AudioClip RespawnSound;
+  public AudioSource audioSource;
+
     private void OnTriggerEnter(Collider collision)
     {
         CheckpointController checkpointController = collision.gameObject.GetComponent<CheckpointController>();
-        
+
         if (checkpointController != null)
         {
             StartCoroutine(ReplacePlayer(checkpointController));
+
         }
     }
 
@@ -18,5 +23,9 @@ public class DeathZone : MonoBehaviour
     {
         yield return new WaitForSeconds(0.8f);
         checkpointController.RespawnEntity();
+      //  yield return new WaitForSeconds(0.8f);
+        audioSource.clip = RespawnSound;
+        audioSource.Play();
+
     }
 }
